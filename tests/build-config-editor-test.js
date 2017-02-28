93ffeae52b8c3cd67488e36d2f0465dd8f27628d
Stephen Vance <steve@vance.com>
2017-02-27 19:37:49 -0500

    Add config block addition and avoidance of duplicate blocks.

--- tests/build-config-editor-test.js
+++ tests/build-config-editor-test.js
@@ -1,10 +1,15 @@
 var EmberBuildConfigEditor = require('../index.js');
 var expect = require('chai').expect;
 var fs = require('fs');
+var astEquality = require('./helpers/esprima-ast-equality.js');
 
-describe('Ember Build Config Editor', function() {
+function readFixture(name) {
+    return fs.readFileSync('./tests/fixtures/' + name, 'utf-8');
+}
+
+describe('Initialization', function() {
     it('parses', function() {
-        var source = fs.readFileSync('./tests/fixtures/default.js');
+        var source = readFixture('default.js');
 
         var build = new EmberBuildConfigEditor(source);
 
@@ -14,4 +19,46 @@ describe('Ember Build Config Editor', function() {
         expect(build.configNode).to.exist;
         expect(build.configNode.type).to.equal('ObjectExpression');
     });
+});
+
+describe('Adds configuration', function() {
+    it('changes nothing if no key is supplied', function() {
+        var source = readFixture('default.js');
+
+        var build = new EmberBuildConfigEditor(source);
+
+        var newBuild = build.edit(undefined, {});
+
+        astEquality(newBuild.code(), source);
+    });
+
+    it('changes nothing if no configuration object is supplied', function() {
+        var source = readFixture('default.js');
+
+        var build = new EmberBuildConfigEditor(source);
+
+        var newBuild = build.edit('some-addon', undefined);
+
+        astEquality(newBuild.code(), source);
+    });
+
+    it('adds an empty configuration if an empty configuration is supplied', function() {
+        var source = readFixture('default.js');
+
+        var build = new EmberBuildConfigEditor(source);
+
+        var newBuild = build.edit('some-addon', {});
+
+        astEquality(newBuild.code(), readFixture('empty-config-block.js'));
+    });
+
+    it('does not duplicate an existing key', function() {
+        var source = readFixture('empty-config-block.js');
+
+        var build = new EmberBuildConfigEditor(source);
+
+        var newBuild = build.edit('some-addon', {});
+
+        astEquality(newBuild.code(), source);
+    })
 });
