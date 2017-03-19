bd051ec62b4cdd85ea249c7da0154ce6626c6146
Stephen Vance <steve@vance.com>
2017-03-19 12:54:52 -0400

    Reformat the code to match the editorconfig.

--- tests/build-config-editor-test.js
+++ tests/build-config-editor-test.js
@@ -4,123 +4,123 @@ var fs = require('fs');
 var astEquality = require('./helpers/esprima-ast-equality.js');
 
 function readFixture(name) {
-    return fs.readFileSync('./tests/fixtures/' + name, 'utf-8');
+  return fs.readFileSync('./tests/fixtures/' + name, 'utf-8');
 }
 
-describe('Initialization', function() {
-    it('parses', function() {
-        var source = readFixture('default.js');
+describe('Initialization', function () {
+  it('parses', function () {
+    var source = readFixture('default.js');
 
-        var build = new EmberBuildConfigEditor(source);
+    var build = new EmberBuildConfigEditor(source);
 
-        expect(build).to.exist;
-        expect(build.source).to.exist;
-        expect(build.ast).to.exist;
-        expect(build.configNode).to.exist;
-        expect(build.configNode.type).to.equal('ObjectExpression');
-    });
+    expect(build).to.exist;
+    expect(build.source).to.exist;
+    expect(build.ast).to.exist;
+    expect(build.configNode).to.exist;
+    expect(build.configNode.type).to.equal('ObjectExpression');
+  });
 });
 
-describe('Adds configuration', function() {
-    it('changes nothing if no key is supplied', function() {
-        var source = readFixture('default.js');
+describe('Adds configuration', function () {
+  it('changes nothing if no key is supplied', function () {
+    var source = readFixture('default.js');
 
-        var build = new EmberBuildConfigEditor(source);
+    var build = new EmberBuildConfigEditor(source);
 
-        var newBuild = build.edit(undefined, {});
+    var newBuild = build.edit(undefined, {});
 
-        astEquality(newBuild.code(), source);
+    astEquality(newBuild.code(), source);
+  });
+
+  it('changes nothing if no configuration object is supplied', function () {
+    var source = readFixture('default.js');
+
+    var build = new EmberBuildConfigEditor(source);
+
+    var newBuild = build.edit('some-addon', undefined);
+
+    astEquality(newBuild.code(), source);
+  });
+
+  it('adds an empty configuration if an empty configuration is supplied', function () {
+    var source = readFixture('default.js');
+
+    var build = new EmberBuildConfigEditor(source);
+
+    var newBuild = build.edit('some-addon', {});
+
+    astEquality(newBuild.code(), readFixture('empty-config-block.js'));
+  });
+
+  it('does not duplicate an existing key', function () {
+    var source = readFixture('empty-config-block.js');
+
+    var build = new EmberBuildConfigEditor(source);
+
+    var newBuild = build.edit('some-addon', {});
+
+    astEquality(newBuild.code(), source);
+  });
+
+  it('adds a key with properties to an empty configuration', function () {
+    var source = readFixture('default.js');
+
+    var build = new EmberBuildConfigEditor(source);
+
+    var newBuild = build.edit('some-addon', {
+      booleanProperty: false,
+      numericProperty: 17,
+      stringProperty: 'wow'
     });
 
-    it('changes nothing if no configuration object is supplied', function() {
-        var source = readFixture('default.js');
+    astEquality(newBuild.code(), readFixture('single-config-block.js'));
+  });
 
-        var build = new EmberBuildConfigEditor(source);
+  it('changes the values of existing configuration properties', function () {
+    var source = readFixture('single-config-block-different-values.js');
 
-        var newBuild = build.edit('some-addon', undefined);
+    var build = new EmberBuildConfigEditor(source);
 
-        astEquality(newBuild.code(), source);
+    var newBuild = build.edit('some-addon', {
+      booleanProperty: false,
+      numericProperty: 17,
+      stringProperty: 'wow'
     });
 
-    it('adds an empty configuration if an empty configuration is supplied', function() {
-        var source = readFixture('default.js');
+    astEquality(newBuild.code(), readFixture('single-config-block.js'));
+  });
 
-        var build = new EmberBuildConfigEditor(source);
+  it('recognizes unquoted properties as matching', function () {
+    var source = readFixture('single-config-block-unquoted-properties.js');
 
-        var newBuild = build.edit('some-addon', {});
+    var build = new EmberBuildConfigEditor(source);
 
-        astEquality(newBuild.code(), readFixture('empty-config-block.js'));
+    var newBuild = build.edit('some-addon', {
+      booleanProperty: false,
+      numericProperty: 17,
+      stringProperty: 'wow'
     });
 
-    it('does not duplicate an existing key', function() {
-        var source = readFixture('empty-config-block.js');
+    astEquality(newBuild.code(), source);
+  });
 
-        var build = new EmberBuildConfigEditor(source);
+  it('recognizes an unquoted key as matching', function () {
+    var source = readFixture('single-config-block-unquoted-key.js');
 
-        var newBuild = build.edit('some-addon', {});
+    var build = new EmberBuildConfigEditor(source);
 
-        astEquality(newBuild.code(), source);
+    var newBuild = build.edit('someaddon', {
+      booleanProperty: false,
+      numericProperty: 17,
+      stringProperty: 'wow'
     });
 
-    it('adds a key with properties to an empty configuration', function() {
-        var source = readFixture('default.js');
-
-        var build = new EmberBuildConfigEditor(source);
-
-        var newBuild = build.edit('some-addon', {
-            booleanProperty: false,
-            numericProperty: 17,
-            stringProperty: 'wow'
-        });
-
-        astEquality(newBuild.code(), readFixture('single-config-block.js'));
-    });
-
-    it('changes the values of existing configuration properties', function() {
-        var source = readFixture('single-config-block-different-values.js');
-
-        var build = new EmberBuildConfigEditor(source);
-
-        var newBuild = build.edit('some-addon', {
-            booleanProperty: false,
-            numericProperty: 17,
-            stringProperty: 'wow'
-        });
-
-        astEquality(newBuild.code(), readFixture('single-config-block.js'));
-    });
-
-    it('recognizes unquoted properties as matching', function() {
-        var source = readFixture('single-config-block-unquoted-properties.js');
-
-        var build = new EmberBuildConfigEditor(source);
-
-        var newBuild = build.edit('some-addon', {
-            booleanProperty: false,
-            numericProperty: 17,
-            stringProperty: 'wow'
-        });
-
-        astEquality(newBuild.code(), source);
-    });
-
-    it('recognizes an unquoted key as matching', function() {
-        var source = readFixture('single-config-block-unquoted-key.js');
-
-        var build = new EmberBuildConfigEditor(source);
-
-        var newBuild = build.edit('someaddon', {
-            booleanProperty: false,
-            numericProperty: 17,
-            stringProperty: 'wow'
-        });
-
-        astEquality(newBuild.code(), source);
-    });
+    astEquality(newBuild.code(), source);
+  });
 });
 
-describe('Retrieves configuration', function() {
-  it('returns undefined if the key is not present', function() {
+describe('Retrieves configuration', function () {
+  it('returns undefined if the key is not present', function () {
     var source = readFixture('default.js');
 
     var build = new EmberBuildConfigEditor(source);
@@ -130,7 +130,7 @@ describe('Retrieves configuration', function() {
     expect(config).to.be.undefined;
   });
 
-  it('returns an empty object when there is an empty config block', function() {
+  it('returns an empty object when there is an empty config block', function () {
     var source = readFixture('empty-config-block.js');
 
     var build = new EmberBuildConfigEditor(source);
@@ -141,7 +141,7 @@ describe('Retrieves configuration', function() {
     expect(Object.keys(config)).to.have.lengthOf(0);
   });
 
-  it('returns the values in the config when present', function() {
+  it('returns the values in the config when present', function () {
     var source = readFixture('single-config-block.js');
 
     var build = new EmberBuildConfigEditor(source);
